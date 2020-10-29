6174424df3fc95253a86f1b0ed0cfe59df216447
Nuno Alexandre <n.alexandre@xgeeks.io>
2020-10-29 08:42:17 +0000

    feat(add): logic to add trnaslation under the current cursor

--- lua/i18nallyvim.lua
+++ lua/i18nallyvim.lua
@@ -1,16 +1,48 @@
 local yaml = require 'yaml'
 
-local function show(word)
+function exists(word)
   local translationFile = io.open("./example/pt_PT.yaml", "r")
   io.input(translationFile)
   local translations = io.read()
   io.close(translationFile)
   local parsedYaml = yaml.parse(translations)
   local currentWord = vim.api.nvim_eval('expand("<cword>")')
-
-  print(parsedYaml[currentWord])
+  if parsedYaml[currentWord] then
+    return true
+  end
+  return false
 end
 
+local function show()
+  local translationFile = io.open("./example/pt_PT.yaml", "r")
+  io.input(translationFile)
+  local translations = io.read()
+  io.close(translationFile)
+  local parsedYaml = yaml.parse(translations)
+  local currentWord = vim.api.nvim_eval('expand("<cword>")')
+  if parsedYaml[currentWord] then
+    print(parsedYaml[currentWord])
+  end
+   print('does not exist!')
+end
+
+local function add()
+  local currentWord = vim.api.nvim_eval('expand("<cword>")')
+  if exists(currentWord) then
+   print("There are already an translation for that word")
+   return;
+  end
+  local translation = vim.api.nvim_eval("input('Translation: ')")
+  local translationFile = io.open("./example/pt_PT.yaml", "a")
+  io.output(translationFile)
+  io.write(currentWord .. ': ' .. translation)
+  io.close(translationFile)
+end
+
+
+
 return {
-  show = show
+  show = show,
+  add = add,
+  exists = exists,
 }
--- plugin/i18nallyvim.vim
+++ plugin/i18nallyvim.vim
@@ -3,5 +3,10 @@ fun! I18nAllyShow()
   lua require('i18nallyvim').show()
 endfun
 
+fun! I18nAllyAdd()
+  lua for k in pairs(package.loaded) do if k:match("^i18nallyvim") then package.loaded[k] = nil end end
+  lua require('i18nallyvim').add()
+endfun
+
 let g:loaded_i18nallyvim = 1
 
